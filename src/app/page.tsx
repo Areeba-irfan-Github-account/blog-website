07541118ffdc886dbd0875e0b68afe7dd0cf4072
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2025-01-03 02:24:10 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
 import HeroSection from "./components/HeroSection";
 import Posts from './posts/page'
 import About from "./components/About";
+import Contact from "./Contact/page";
+
 
 export default function Home() {
   return (
-   <>
-     <HeroSection />
+    <>
+
+      <HeroSection />
       <Posts />
       <About />
-   </>
+      <Contact />
+
+
+
+    </>
   );
 }
